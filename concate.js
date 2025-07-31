a44286753f94970f267935e89dd0b882c92fed12
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-31 11:14:30 -0500

    concate added

---
+++ concate.js
@@ -0,0 +1,10 @@
+let myName="Gaurav";
+let herName="Antara";
+let loveName=myName.concat("+").concat(herName);
+console.log(loveName);
+
+let list=["Apple","Guava","Grape"];
+let list2=["Bokul full","Belly full","Joba full"];
+
+let folFull=list.concat(list2);
+console.log(folFull);
