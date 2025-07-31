61e728f86760492c307fb6b9d3ec1d7881a839bf
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-31 10:35:41 -0500

    string added

---
+++ stringdec.js
@@ -0,0 +1,8 @@
+let device='Computer';
+let  CPU="Ryzen AMD 7600";
+let motherboard=`MSI`;
+let Graphics=new String('Zotac')
+console.log("Device=",device,"CPU=",CPU,"Motherboard=",motherboard,"Graphics=",Graphics);
+console.log(device[2]);
+console.log(typeof Graphics);
+console.log(CPU.length);
---
+++ trim.js
@@ -0,0 +1,9 @@
+let nam=" Zunayed";
+let firstPosition="zunayed "
+
+if(nam.toLowerCase().trim()===firstPosition.toLowerCase().trim()){
+    console.log("Give him first prize.");
+}
+else{
+    console.log("No prize");
+}
