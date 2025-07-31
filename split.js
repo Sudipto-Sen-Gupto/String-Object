41fa041868ad3d511b75b8293902443c6904b59a
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-31 11:03:40 -0500

    split added

---
+++ split.js
@@ -0,0 +1,6 @@
+let information="He joined programming hero as alumni.Now he is an instructor of programming hero."
+
+let Misinformation=information.split(" ");
+console.log(Misinformation);
+let list=["Apple","Orange","Litchi","Jackfruit"];
+console.log(list.join('|'));
